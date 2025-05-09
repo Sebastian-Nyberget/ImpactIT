a356630ed3dc8ea74beed71306d8fe36b6eea4b4
Sebastian-Nyberget <nybesebe@gmail.com>
2025-05-09 10:35:17 +0200

    Cookie consent

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type React from "react";
 import "@/app/globals.css";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
+import Script from "next/script";
+import { IMPACTIT_CONFIG } from "@/lib/utils";
 
 export const metadata = {
   title: "ImpactIT - Profesjonell IT-utstyr",
@@ -17,7 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-          {children}
+        {IMPACTIT_CONFIG.isProd && (
+          <Script
+            id="CookieConsent"
+            src="https://policy.app.cookieinformation.com/uc.js"
+            data-culture="NB"
+            data-gcm-version="2.0"
+            type="text/javascript"
+          />
+        )}
+        {children}
       </body>
     </html>
   );
